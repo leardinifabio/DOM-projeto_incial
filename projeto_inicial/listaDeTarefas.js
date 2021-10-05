5c4e4f206972d6de5452922379608851058e2f50
Fabio Leardini <leardinifabio90@gmail.com>
2021-10-04 21:41:45 -0300

    add informações de forma dinamica

--- projeto_inicial/listaDeTarefas.js
+++ projeto_inicial/listaDeTarefas.js
@@ -1,12 +1,24 @@
+//querySelector percorre na árvore do DOM
+//createElement manipula elementos na árvore do DOM
+
 const criarTarefa = (evento) => {
 
-    evento.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-    console.log(valor);
+    evento.preventDefault()
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+
+    const conteudo = `<p class="content">${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    lista.appendChild(tarefa)
     input.value = ''
 }
 
-const novaTarefa = document.querySelector('[data-form-button]');
+const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
